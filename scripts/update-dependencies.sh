c165d63ea92896e928ae09c89c887780306acd7d
shimataro <shimataro@zelkova.cc>
2019-09-22 08:47:04 +0900

    Feature/scripts (#18)
    * * add rebuild script
    
    * * add update-dependencies script

---
+++ scripts/update-dependencies.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# update dependencies
+
+DATE=$(date +"%Y%m%d")
+BRANCH=feature/update-dependencies-${DATE}
+COLOR_SUCCESS="\e[1;32m"
+COLOR_RESET="\e[m"
+
+# create branch
+git checkout development || exit 1
+git checkout -b ${BRANCH} || exit 1
+
+# check updates
+npm ci
+npm run check-updates -- -u || exit 1
+
+# re-install packages
+rm -rf package-lock.json node_modules || exit 1
+npm i || exit 1
+
+# build check
+npm run build || exit 1
+
+# commit
+npm ci --only=production || exit 1
+git add package.json package-lock.json node_modules || exit 1
+git commit -m "update dependencies" || exit 1
+
+# finished!
+echo -e "
+${COLOR_SUCCESS}🎉All dependencies are updated successfully.🎉${COLOR_RESET}
+
+Push changes and merge into 'develop' branch.
+
+    git push --set-upstream origin ${BRANCH}
+"
